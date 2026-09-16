78174a6d995720b646eb17d21aacab623e2a6f5d
Marcos_Vinicius <vinifilho58@gmail.com>
2026-09-16 19:44:47 +0000

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # projeto-front-TED
+
---
+++ main.js
@@ -0,0 +1,7 @@
+let buttonCe = document.getElementById("ce")
+
+buttonCe.addEventListener('click', function(){
+    const novaDiv = document.createElement("div")
+    novaDiv.textContent = "Sutil + Vitoria = ♡ "
+     document.body.appendChild(novaDiv)
+})
